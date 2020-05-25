9d7998bdc0bb453b06e73bd655ae9b3609ebe96c
porosh2002 <jamilkashem2002@gmail.com>
2020-05-25 22:37:45 +0600

    Updates

---
+++ precache-manifest.7dd57dd1498b7a19da71d3e779ef37af.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f96ffacf563aba10dad05f16b2ec837",
+    "url": "/beach-resort/index.html"
+  },
+  {
+    "revision": "32b55cf4a967bb46aa6c",
+    "url": "/beach-resort/static/css/main.13630a62.chunk.css"
+  },
+  {
+    "revision": "b3e446448c35df29d7ed",
+    "url": "/beach-resort/static/js/2.8a480c5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beach-resort/static/js/2.8a480c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b55cf4a967bb46aa6c",
+    "url": "/beach-resort/static/js/main.425ca651.chunk.js"
+  },
+  {
+    "revision": "75a12d460423afb3ec0e",
+    "url": "/beach-resort/static/js/runtime-main.a5a00b07.js"
+  },
+  {
+    "revision": "d01731eecbeb065d79d807dacb623a93",
+    "url": "/beach-resort/static/media/bg_pattern.d01731ee.png"
+  },
+  {
+    "revision": "cb86bfd692e2bfdcb9df21fc77f6237c",
+    "url": "/beach-resort/static/media/details-1.cb86bfd6.jpeg"
+  },
+  {
+    "revision": "92d9572b63ae60cb10a219f5f292ac6d",
+    "url": "/beach-resort/static/media/details-2.92d9572b.jpeg"
+  },
+  {
+    "revision": "c1e029e27f4c705598cc41a03e5d4663",
+    "url": "/beach-resort/static/media/details-3.c1e029e2.jpeg"
+  },
+  {
+    "revision": "8c8c053e77de0c0520da28df8e73069b",
+    "url": "/beach-resort/static/media/details-4.8c8c053e.jpeg"
+  },
+  {
+    "revision": "d00c6d48f502ff93cf89bf6d41640270",
+    "url": "/beach-resort/static/media/hotel.d00c6d48.jpg"
+  },
+  {
+    "revision": "8aa89655a9a18838a0ff7ad56c8a46a7",
+    "url": "/beach-resort/static/media/room-1.8aa89655.jpeg"
+  },
+  {
+    "revision": "23960e49a3718cb27842ae012f413da0",
+    "url": "/beach-resort/static/media/room-10.23960e49.jpeg"
+  },
+  {
+    "revision": "01b7825eff247312a966b8f6ce68ac7c",
+    "url": "/beach-resort/static/media/room-11.01b7825e.jpeg"
+  },
+  {
+    "revision": "2f134677eac5d73135bce0ede052e375",
+    "url": "/beach-resort/static/media/room-12.2f134677.jpeg"
+  },
+  {
+    "revision": "f1b6de7629376e29f9414b3bf8fc6a41",
+    "url": "/beach-resort/static/media/room-2.f1b6de76.jpeg"
+  },
+  {
+    "revision": "b273b75654497512d9e837a199327b4d",
+    "url": "/beach-resort/static/media/room-3.b273b756.jpeg"
+  },
+  {
+    "revision": "bfbb444ddecec415f68727dc81ce7bbe",
+    "url": "/beach-resort/static/media/room-4.bfbb444d.jpeg"
+  },
+  {
+    "revision": "f3b5b898497a3b5d19b66869d952b5e8",
+    "url": "/beach-resort/static/media/room-5.f3b5b898.jpeg"
+  },
+  {
+    "revision": "e8ffe778548cb9ba8ffd27fa585f981c",
+    "url": "/beach-resort/static/media/room-6.e8ffe778.jpeg"
+  },
+  {
+    "revision": "a78f8ba2f2a1df1e17aa9105f7967613",
+    "url": "/beach-resort/static/media/room-7.a78f8ba2.jpeg"
+  },
+  {
+    "revision": "d1485c8246fcc2a2f91e21d4e1668972",
+    "url": "/beach-resort/static/media/room-8.d1485c82.jpeg"
+  },
+  {
+    "revision": "f0f306254e8ade99b503e6d3c3094081",
+    "url": "/beach-resort/static/media/room-9.f0f30625.jpeg"
+  },
+  {
+    "revision": "06d75777e1898c42407cf4c6613976ec",
+    "url": "/beach-resort/static/media/room.06d75777.jpg"
+  }
+]);
